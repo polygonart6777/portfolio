0c146ef86adea5b63573dc36224328044f7d3d8c
polygonart6777 <buricd6777@gmail.com>
2022-07-06 20:22:54 -0700

    Adding overflow scroll for mobile

--- src/components/Torus.tsx
+++ src/components/Torus.tsx
@@ -1,27 +1,28 @@
-import React from "react";
+import React from 'react';
 
-export default function Torus() {
+interface TorusProps {
+  isMobile: boolean;
+}
+
+export default function Torus({ isMobile }: TorusProps) {
   const [opacity, setOpacity] = React.useState(0.5);
+
   return (
     <>
       <mesh
         position={[0, 0, 0]}
         rotation={[-5, 10, 3]}
         onClick={() => {
-          setOpacity(0.7);
+          isMobile && setOpacity(0.7);
         }}
         onPointerOver={() => {
-          setOpacity(0.7);
+          isMobile && setOpacity(0.7);
         }}
         onPointerOut={() => {
           setOpacity(0.5);
         }}
       >
-        <torusBufferGeometry
-          attach="geometry"
-          args={[8, 2.6, 20, 80]}
-          name="torus"
-        />
+        <torusBufferGeometry attach="geometry" args={[8, 2.6, 20, 80]} name="torus" />
         <meshNormalMaterial opacity={opacity} transparent />
       </mesh>
     </>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,126 +1,114 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
-import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
-import Hidden from "@mui/material/Hidden";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import IconButton from "@mui/material/IconButton";
-import Box from "@mui/material/Box";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { HashLink } from "react-router-hash-link";
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Hidden from '@mui/material/Hidden';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { HashLink } from 'react-router-hash-link';
 
-import Social from "../components/Social";
-import Torus from "../components/Torus";
+import Social from '../components/Social';
+import Torus from '../components/Torus';
 
 const layoutStyle = {
-  color: "white",
-  display: "flex",
-  alignItems: "center",
-  displayContent: "center",
-  width: "100%",
-  height: "90vh",
-  zIndex: "10",
+  color: 'white',
+  display: 'flex',
+  alignItems: 'center',
+  displayContent: 'center',
+  width: '100%',
+  height: '90vh',
+  zIndex: '10',
+  overflow: 'scroll',
 };
 
 const gridStyle = {
-  position: "absolute",
-  fontWeight: "medium",
-  paddingLeft: "10rem",
-  displayContent: "center",
-  top: "14rem",
-  "@media (max-width:650px)": { top: "10rem", paddingLeft: "2rem" },
-  zIndex: "10",
+  position: 'absolute',
+  fontWeight: 'medium',
+  paddingLeft: '10rem',
+  displayContent: 'center',
+  top: '14rem',
+  '@media (max-width:650px)': { top: '10rem', paddingLeft: '2rem' },
+  zIndex: '10',
 };
 
 const titleStyle = {
-  fontWeight: "bold",
-  zIndex: "5",
+  fontWeight: 'bold',
+  zIndex: '5',
 };
 
 const canvasStyle = {
-  height: "100%",
-  width: "100%",
-  overflow: "scroll",
+  height: '100%',
+  width: '100%',
+  overflow: 'scroll',
 };
+
 const subtitleStyle = {
-  textAlign: "start",
-  zIndex: "10",
-  "@media (max-width:650px)": { width: "200px" },
-  wordWrap: "break-word",
+  textAlign: 'start',
+  zIndex: '10',
+  '@media (max-width:650px)': { width: '200px' },
+  wordWrap: 'break-word',
 };
 
 export default function Home() {
-  const isMobile = useMediaQuery("(max-width:650px)");
+  const isMobile = useMediaQuery('(max-width:650px)');
   return (
     <>
       <div style={layoutStyle}>
-        <Grid
-          container
-          direction="row"
-          justifyContent="center"
-          sx={gridStyle}
-          rowSpacing={1}
-        >
+        <Grid container direction="row" justifyContent="center" sx={gridStyle} rowSpacing={1}>
           <Grid item xs={12}>
-            <Typography
-              variant="h2"
-              component="h2"
-              color="#71C0D8"
-              sx={titleStyle}
-            >
+            <Typography variant="h2" component="h2" color="#71C0D8" sx={titleStyle}>
               Dr. Dina Buric
             </Typography>
           </Grid>
           <Grid item xs={12}>
-            <Typography
-              variant="h5"
-              component="h2"
-              color="white"
-              sx={subtitleStyle}
-            >
+            <Typography variant="h5" component="h2" color="white" sx={subtitleStyle}>
               Software developer and math artist
             </Typography>
           </Grid>
         </Grid>
         <Canvas style={canvasStyle}>
-          <OrbitControls enableZoom={false} enabled={isMobile ? false : true} />
+          <OrbitControls enableZoom={false} enabled={false} />
           <ambientLight intensity={0.5} />
           <directionalLight position={[-2, 5, 2]} intensity={1} />
-          <Torus></Torus>
+          <Torus isMobile={isMobile}></Torus>
         </Canvas>
       </div>
       <Hidden smDown>
         <Social />
       </Hidden>
-      <Box
-        component="div"
-        sx={{
-          position: "absolute",
-          bottom: "2rem",
-          left: "11rem",
-          zIndex: "100",
-        }}
-      >
-        <Hidden smUp>
+      <Hidden smUp>
+        <Box
+          component="div"
+          sx={{
+            position: 'absolute',
+            bottom: '1rem',
+            left: '11rem',
+            zIndex: '100',
+          }}
+        >
           <HashLink smooth to={`#experience`}>
             <IconButton
               size="large"
               sx={{
-                backgroundColor: "#71C0D8",
-                opacity: ".75",
-                "&:hover": {
-                  backgroundColor: "rgb(255,0,255,.5)",
-                  transition: "all 0.5s ease",
+                backgroundColor: '#71C0D8',
+                opacity: '.75',
+                '&:hover': {
+                  backgroundColor: '#71C0D8',
+                },
+                '&:selected': {
+                  backgroundColor: '#71C0D8',
                 },
               }}
             >
-              <ExpandMoreIcon sx={{ color: "black)" }} />
+              <ExpandMoreIcon sx={{ color: 'black)' }} />
             </IconButton>
           </HashLink>
-        </Hidden>
-      </Box>
+        </Box>
+      </Hidden>
     </>
   );
 }
